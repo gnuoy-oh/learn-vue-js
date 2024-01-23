43f2de82d44bd87f20e0100755ef84d3775dc5f7
ohmy04180 <ohmy0418@mobigen.com>
2024-01-23 15:51:06 +0900

    feat: vue3-form add

--- vue3-form/src/App.vue
+++ vue3-form/src/App.vue
@@ -1,68 +1,47 @@
 <template>
   <form action="" @submit.prevent="submitForm">
     <div>
-      <label for="userId">ID:</label>
-      <input id="userId" type="text" v-model="username">
+      <!-- v-model: 뷰의 디렉티브 중 하나로, 값을 받아와서 동기화 시킬 수 있는 디렉티브  -->
+      <label for="userId">ID: </label>
+      <input type="text" id="userId" v-model="username" />
     </div>
     <div>
-      <label for="password">PW:</label>
-      <input type="text" v-model="password">
+      <label for="password"> PW: </label>
+      <input type="text" id="password" v-model="password" />
     </div>
+    <!-- @click 이벤트를 넣어도 되지만, form 요소에서 제어해도 된다. -->
     <button type="submit">로그인</button>
   </form>
 </template>
 
 <script>
-import axios from 'axios'
-import { ref } from 'vue';
+import axios from "axios";
 
-  export default {
-    setup() {
-      // data
-      var username = ref('');
-      var password = ref('');
+export default {
+  data() {
+    return {
+      username: "",
+      password: ""
+    };
+  },
+  methods: {
+    submitForm() {
+      // 상위 디렉티브에서 이벤트 제어를 할 수 있음 (@click.prevent="submitForm")
+      // event.preventDefault();
 
-      // methods
-      var submitForm = () => {
-        axios.post('https://jsonplaceholder.typicode.com/users/', {
-          username: username.value,
-          password: password.value
-        }).then(response => {
+      const data = {
+        username: this.username,
+        password: this.password
+      };
+
+      axios
+        .post("https://jsonplaceholder.typicode.com/users", data)
+        .then(response => {
           console.log(response);
-        })
-      }
-      
-      return { username, password, submitForm }
-    },
-    // methods: {
-    //   logText() {
-    //     this.pas
-    //   }
-    // }
-
-    // data() {
-    //   return {
-    //     username: '',
-    //     password: '',
-    //   }
-    // },
-    // methods: {
-    //   submitForm() {
-    //     // event.preventDefault();
-    //     const data = {
-    //       username: this.username,
-    //       password: this.password,
-    //     }
-    //     axios.post('https://jsonplaceholder.typicode.com/users/', data)
-    //       .then(response => {
-    //         console.log(response)
-    //       });
-    //     // console.log('제출됨')
-    //   }
-    // }
+        });
+    }
   }
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
